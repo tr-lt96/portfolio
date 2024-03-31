cc0964f6c0a7d7e2015a7fe1dc64cc64bb515995
Hannah-La <hanh.kimla@gmail.com>
2024-03-31 22:53:58 +1000

    chore: remove svg

--- src/_components/landing/Hero.jsx
+++ src/_components/landing/Hero.jsx
@@ -7,12 +7,8 @@ export default function Hero() {
   return (
     <div className="hero-bg m-auto h-screen w-full relative">
       <div className="flex justify-center h-full items-center">
-        {/* <Image src="/hero-bg.png" alt="" fill className={"z-0 opacity-10 w-screen invert"} style={{ objectFit: "cover" }} /> */}
         <LandingSection>
           <div className='z-10 flex flex-col w-full justify-center gap-y-4'>
-            {/* <div className='w-40 h-40 relative'>
-              <Image src="/profile.jpg" alt="" fill style={{ objectFit: 'cover' }} className='rounded-full border-2 border-p-charcoal' />
-            </div> */}
             <div className={`typewriter-text text-3xl md:text-[48px] leading-tight text-p-cinnabar font-semibold text-nowrap max-w-fit`}>
               {`Hi, I'm `}
               <span className='text-p-cornsilk pr-4'>Luke Tran</span>
