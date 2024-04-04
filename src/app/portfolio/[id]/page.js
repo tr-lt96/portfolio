05616160f33c7d49e93c6096a9bc18c267c72624
Luke <149994784+tr-lt96@users.noreply.github.com>
2024-04-04 22:45:49 +1000

    fix: fix static rendering stire (#3)

--- src/app/portfolio/[id]/page.js
+++ src/app/portfolio/[id]/page.js
@@ -3,11 +3,22 @@
 import { ArrowLeft } from "@/_components/icon";
 import PORTFOLIO from "@/_data/portfolio";
 import { fontSecondary } from "@/_utils/fonts";
-import { useParams, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 
-export default function Page() {
-  const { id } = useParams();
-  const portfolio = id !== undefined && id !== null ? PORTFOLIO[id - 1] || null : null;
+export function generateStaticPaths() {
+  return {
+    paths: PORTFOLIO.map((_portfolio, index) => ({
+      params: {id: `${index + 1}`}
+    }))
+  }
+}
+
+export default function Page({ params }) {
+  const portfolioId = parseInt(params?.id)
+  const portfolio =
+    isNaN(portfolioId) || portfolioId < 1 || portfolioId > PORTFOLIO.length
+      ? undefined
+      : PORTFOLIO[portfolioId - 1]
   const router = useRouter();
   const goBack = () => router.push("/");
 
@@ -17,11 +28,11 @@ export default function Page() {
 
   return (
     <div className="flex flex-col items-center w-full">
-      <div className="w-screen h-[250px] bg-p-cinnabar block"></div>
+      <div className="w-screen h-[250px] bg-p-charcoal/50 block"></div>
       <div className="max-w-[1000px] w-full px-8 md:px-12 mt-16">
         <button type="button" onClick={goBack} className="flex flex-row items-center">
-            <ArrowLeft className={'w-4 h-4 text-p-cornsilk fill-p-cornsilk mr-2'}/>
-            Back
+          <ArrowLeft className={'w-4 h-4 text-p-cornsilk fill-p-cornsilk mr-2'} />
+          Back
         </button>
         <h1 className={`${fontSecondary.className} uppercase mt-8`}>{portfolio.title}</h1>
         <p className="mt-8">{portfolio.description}</p>
